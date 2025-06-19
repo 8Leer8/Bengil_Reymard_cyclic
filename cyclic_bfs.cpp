34ec66d47bba41d52cd969e77337606b76b35edc
Mard <bengilreymard@gmail.com>
2025-06-20 06:34:26 +0800

    clean and more visual cyclic update

--- cyclic_bfs.cpp
+++ cyclic_bfs.cpp
@@ -1,26 +1,50 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
 const int MAX = 100;
 int graph[MAX][MAX];
 int parent[MAX];
+bool visited[MAX];
 int n;
 
-int find(int i) {
-    while (parent[i] != -1) {
-        i = parent[i];
-    }
-    return i;
-}
+bool bfs(int start) {
+    queue<int> q;
+    visited[start] = true;
+    parent[start] = -1;
+    q.push(start);
 
-bool unionSet(int u, int v) {
-    int pu = find(u);
-    int pv = find(v);
-    if (pu == pv) {
-        return false;
+    while (!q.empty()) {
+        int u = q.front(); q.pop();
+
+        for (int v = 0; v < n; v++) {
+            if (graph[u][v]) {
+                if (!visited[v]) {
+                    visited[v] = true;
+                    parent[v] = u;
+                    q.push(v);
+                } else if (v != parent[u]) {
+                    cout << "Cycle detected: ";
+                    int path[MAX], len = 0;
+                    path[len++] = v;
+                    int cur = u;
+                    while (cur != -1 && cur != v && len < MAX) {
+                        path[len++] = cur;
+                        cur = parent[cur];
+                    }
+                    path[len++] = v;
+                    for (int i = len - 1; i >= 0; i--) {
+                        cout << path[i];
+                        if (i > 0) cout << " -> ";
+                    }
+                    cout << endl;
+                    return true;
+                }
+            }
+        }
     }
-    parent[pu] = pv;
-    return true;
+
+    return false;
 }
 
 int readIntCell(int i, int j) {
@@ -28,13 +52,12 @@ int readIntCell(int i, int j) {
     while (true) {
         cout << "Enter value for row " << i << " column " << j << " (0 or 1): ";
         cin >> value;
-
         if (cin.fail()) {
             cin.clear();
             cin.ignore(1000, '\n');
-            cout << "Invalid input! Please enter a number.\n";
+            cout << "Invalid input!! Please enter a number.\n";
         } else if (value != 0 && value != 1) {
-            cout << "Invalid value! Only 0 or 1 allowed.\n";
+            cout << "Invalid value!! Only 0 or 1 allowed.\n";
         } else {
             return value;
         }
@@ -46,44 +69,32 @@ int main() {
     while (!(cin >> n) || n <= 0 || n > MAX) {
         cin.clear();
         cin.ignore(1000, '\n');
-        cout << "Invalid input! Enter a valid number between 1 and " << MAX << ": ";
+        cout << "Invalid input!! Enter a valid number between 1 and " << MAX << ": ";
     }
 
     cout << "\nEnter adjacency matrix:\n";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++){
         cout << "\n--- Row " << i << " ---\n";
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < n; j++)
             graph[i][j] = readIntCell(i, j);
-        }
     }
-
+    // Initialize visited
     for (int i = 0; i < n; i++) {
+        visited[i] = false;
         parent[i] = -1;
     }
 
     bool hasCycle = false;
-    int u = -1, v = -1;
-
     for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (graph[i][j]) {
-                if (!unionSet(i, j)) {
-                    hasCycle = true;
-                    u = i;
-                    v = j;
-                    break;
-                }
+        if (!visited[i]) {
+            if (bfs(i)) {
+                hasCycle = true;
+                break;
             }
-        }
-        if (hasCycle) {
-            break;
         }
     }
 
-    cout << "\n--- BFS (Union-Find) Result ---\n";
-    if (hasCycle) {
-        cout << "Cycle detected between vertices: " << u << " and " << v << endl;
-    } else {
+    if (!hasCycle) {
         cout << "No cycle found using BFS.\n";
     }
 
--- cyclic_dfs.cpp
+++ cyclic_dfs.cpp
@@ -4,19 +4,28 @@ using namespace std;
 const int MAX = 100;
 int graph[MAX][MAX];
 int visited[MAX];
-int n;
+int parent[MAX];
+int cyclePath[MAX];
+int n, cycleLength;
 
-bool dfs(int u, int parent, int &start, int &end) {
+bool dfs(int u) {
     visited[u] = 1;
     for (int v = 0; v < n; v++) {
         if (graph[u][v]) {
             if (!visited[v]) {
-                if (dfs(v, u, start, end)) {
+                parent[v] = u;
+                if (dfs(v)){
                     return true;
+                } 
+            } else if (v != parent[u]) {
+                cycleLength = 0;
+                int cur = u;
+                cyclePath[cycleLength++] = v;
+                while (cur != v && cycleLength < MAX) {
+                    cyclePath[cycleLength++] = cur;
+                    cur = parent[cur];
                 }
-            } else if (v != parent) {
-                start = u;
-                end = v;
+                cyclePath[cycleLength++] = v;
                 return true;
             }
         }
@@ -29,13 +38,12 @@ int readIntCell(int i, int j) {
     while (true) {
         cout << "Enter value for row " << i << " column " << j << " (0 or 1): ";
         cin >> value;
-
         if (cin.fail()) {
             cin.clear();
             cin.ignore(1000, '\n');
-            cout << "Invalid input! Please enter a number.\n";
+            cout << "Invalid input!! Please enter a number.\n";
         } else if (value != 0 && value != 1) {
-            cout << "Invalid value! Only 0 or 1 allowed.\n";
+            cout << "Invalid value!! Only 0 or 1 allowed.\n";
         } else {
             return value;
         }
@@ -47,7 +55,7 @@ int main() {
     while (!(cin >> n) || n <= 0 || n > MAX) {
         cin.clear();
         cin.ignore(1000, '\n');
-        cout << "Invalid input! Enter a valid number between 1 and " << MAX << ": ";
+        cout << "Invalid input!! Enter a valid number between 1 and " << MAX << ": ";
     }
 
     cout << "\nEnter adjacency matrix:\n";
@@ -60,14 +68,15 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         visited[i] = 0;
+        parent[i] = -1;
     }
 
     bool hasCycle = false;
-    int start = -1, end = -1;
+    cycleLength = 0;
 
     for (int i = 0; i < n; i++) {
         if (!visited[i]) {
-            if (dfs(i, -1, start, end)) {
+            if (dfs(i)) {
                 hasCycle = true;
                 break;
             }
@@ -76,7 +85,12 @@ int main() {
 
     cout << "\n--- DFS Result ---\n";
     if (hasCycle) {
-        cout << "Cycle detected between vertices: " << start << " and " << end << endl;
+        cout << "Cycle detected: ";
+        for (int i = cycleLength - 1; i >= 0; i--) {
+            cout << cyclePath[i];
+            if (i > 0) cout << " -> ";
+        }
+        cout << endl;
     } else {
         cout << "No cycle found using DFS.\n";
     }
